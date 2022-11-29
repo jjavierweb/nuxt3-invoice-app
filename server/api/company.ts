efcb926eecf59e070b19442f826df058c6888eb3
Javier Gutierrez <jjavierweb@outlook.com>
2022-11-29 16:43:58 -0500

    Added check for user authentication

--- server/api/company.ts
+++ server/api/company.ts
@@ -4,6 +4,9 @@ import { serverSupabaseUser } from "#supabase/server";
 export default defineEventHandler(async (event) => {
   const prisma = new PrismaClient();
   const user = await serverSupabaseUser(event);
+  if (!user) {
+    throw createError({ statusCode: 401, message: "Unauthorized" });
+  }
   const companies = await prisma.company.findFirst({
     where: {
       user: user?.id,
